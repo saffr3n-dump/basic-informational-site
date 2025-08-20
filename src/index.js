5996092b2592aea5a2323b3dc0f66665639e16c8
saffr3n <saffr3n@gmail.com>
2025-08-20 15:33:32 +0300

    refactor: rewrite using express

--- src/index.js
+++ src/index.js
@@ -1,41 +1,24 @@
-import http from 'http';
-import fs from 'fs/promises';
+import express from 'express';
 import path from 'path';
 
+const app = express();
+const publicDir = path.resolve(import.meta.dirname, '../public');
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html', { root: publicDir });
+});
+app.get('/about', (req, res) => {
+  res.sendFile('about.html', { root: publicDir });
+});
+app.get('/contact', (req, res) => {
+  res.sendFile('contact.html', { root: publicDir });
+});
+app.use((req, res) => {
+  res.status(404).sendFile('404.html', { root: publicDir });
+});
+
 const PORT = 3000;
-
-http
-  .createServer(async (req, res) => {
-    let page;
-    let status = 200;
-
-    try {
-      switch (req.url) {
-        case '/':
-          page = await readPage('index.html');
-          break;
-        case '/about':
-          page = await readPage('about.html');
-          break;
-        case '/contact':
-          page = await readPage('contact.html');
-          break;
-        default:
-          page = await readPage('404.html');
-          status = 404;
-      }
-    } catch (err) {
-      console.error(err);
-    }
-
-    res.writeHead(status, { 'content-type': 'text/html' });
-    res.end(page);
-  })
-  .listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
-function readPage(filename) {
-  return fs.readFile(
-    path.resolve(import.meta.dirname, '..', 'public', filename),
-    { encoding: 'utf8' },
-  );
-}
+app.listen(3000, (err) => {
+  if (err) throw err;
+  console.log(`Server started on port ${PORT}`);
+});
